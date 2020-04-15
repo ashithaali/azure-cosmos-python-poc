b6ea1692136f2d086ed26aae9551455ee0b788ff
ashithaali <55775626+ashithaali@users.noreply.github.com>
2020-04-15 12:00:08 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 envVariables = {
-    'host': 'https://0d4819e8-0ee0-4-231-b9ee.documents.azure.com:443/',
-    'master_key': 'wArxhxAZdwznkrwOtWNhcsaSju2dECiqBOlJvWtRzUSIveTIMWL1F0zMLp3yMLG9KzwBnuEMxJenOgMmoMLFwg=='
+    'host': 'https://f0339f55-0ee0-4-231-b9ee.documents.azure.com:443/',
+    'master_key': 'uFYLezcXlUhxxj5J5Av55ls6UM0jb4D2nqkXNFBMqPMElM6VyliXpXodVobhKQCRfvcgnQrVcaWcx7B7eR1q4Q=='
 }
